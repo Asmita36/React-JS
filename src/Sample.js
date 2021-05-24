4da44c3b3b6fe96f0e294a7ee2a60c63a8e7252f
Asmita36 <asmitatotre1050@gmail.com>
2021-05-24 17:18:41 +0530

    Test Challebge

---
+++ src/Sample.js
@@ -0,0 +1,53 @@
+
+import './Sample.css'
+
+function Sample() {
+  return (
+  
+      <div>
+
+<div className="Sample12">
+      <p>
+      <div><img src="/image/Entrance.jpg" alt=""/></div>
+      </p>
+      
+</div>
+    <div className="Sample1">
+        <h3> Left Shopping Window</h3>
+        <h5>12 Accessories</h5>
+        <button type="button" classname="buttonliston">ON</button>
+        
+
+        </div>
+      <div className="Sample2">
+      <h3> Right Shopping Window</h3>
+       <h5>9 Accessories</h5>
+        <button type="button" classname="buttonliston">ON</button>
+ 
+     </div>
+        <div className="Sample3">
+        <h3> Front Entrance</h3>
+        <h5>12 Accessories</h5>
+        <button type="button" classname="buttonliston">ON</button>
+
+
+
+        </div>
+        <div className="Sample4">
+        <h3> Shoe Wall</h3>
+        <h5>20 Accessories</h5>
+        <button type="button" classname="buttonlistoff">OFF</button>
+        
+
+    </div> 
+    
+
+
+</div>
+
+  );
+}
+
+
+
+export default Sample;
